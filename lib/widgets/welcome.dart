f1451d2292db25c6123abeeb1fa42cc13411a9d8
Shell Raiser <ksshailesh05@gmail.com>
2022-08-06 15:11:17 +0530

    revert back to og

--- lib/widgets/welcome.dart
+++ lib/widgets/welcome.dart
@@ -14,26 +14,50 @@ class _WelcomeState extends State<Welcome> {
     return Scaffold(
       body: Container(
         // Another way to set the background color
-        decoration: BoxDecoration(color: Color.fromARGB(221, 70, 225, 153)),
+        decoration: const BoxDecoration(
+            color: Color.fromARGB(221, 0, 255, 255),
+            image: DecorationImage(
+                image: AssetImage("images/natureBG.png"), fit: BoxFit.cover)),
         child: Center(
-          child: Container(
-            child: Text(
-              'Saviour',
-              // Set text style as per theme
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 40),
+            child: Container(
+              height: 100,
+          color: Color.fromARGB(57, 0, 0, 0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Saviour",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 40),
+                ),
+                Text(
+                  'An app to save the environment',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20),
+                ),
+              ]),
+        )
+            // child: Text(
+            //   'Saviour',
+            //   // Set text style as per theme
+            //   style: TextStyle(
+            //       fontWeight: FontWeight.bold,
+            //       color: Color.fromARGB(255, 255, 255, 255),
+            //       fontSize: 40),
+            // ),
             ),
-          ),
-        ),
       ),
       floatingActionButton: Theme(
         // override the accent color of theme for this widget only
         data: Theme.of(context).copyWith(
-          colorScheme: Theme.of(context)
-              .colorScheme
-              .copyWith(secondary: Color.fromARGB(255, 0, 0, 0)),
+          colorScheme: Theme.of(context).colorScheme.copyWith(
+              secondary: Color.fromARGB(255, 255, 255, 0),
+              onSecondary: Color.fromARGB(255, 0, 0, 0)),
         ),
         child: FloatingActionButton(
             onPressed: () {
