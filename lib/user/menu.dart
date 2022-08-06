c3d51cb1d7e7b3f19493005e66bc1d59815bd7d6
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-08-06 16:16:59 +0530

    added the cards for the menu page

--- lib/user/menu.dart
+++ lib/user/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:saviour/user/ngolists.dart';
+import 'package:saviour/widgets/home.dart';
 
 // ignore: camel_case_types
 class menu extends StatefulWidget {
@@ -13,31 +15,101 @@ class menuState extends State<menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text('User Options'),
-        ),
-        body: Center(
-          child: ListView(
-            padding: const EdgeInsets.all(8),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('User Options'),
+      ),
+      body: Column(children: [
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Container(
-                height: 50,
-                color: Colors.amber[600],
-                child: const Center(child: Text('Entry A')),
+              const ListTile(
+                leading: Icon(
+                  Icons.playlist_add_check_circle,
+                  size: 50,
+                ),
+                title: Text('Complaint status'),
+                subtitle: Text('Check your complaint Status here.'),
               ),
-              Container(
-                height: 50,
-                color: Colors.amber[500],
-                child: const Center(child: Text('Entry B')),
-              ),
-              Container(
-                height: 50,
-                color: Colors.amber[100],
-                child: const Center(child: Text('Entry C')),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text(
+                      'Check',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                ],
               ),
             ],
           ),
-        ));
+        ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(
+                  Icons.drive_file_rename_outline_sharp,
+                  size: 50,
+                ),
+                title: Text('Raise Complaint'),
+                subtitle: Text('Here you can raise new compliant.'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text(
+                      'Add',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(
+                  Icons.attach_money,
+                  size: 50,
+                ),
+                title: Text('Donation'),
+                subtitle: Text('Help NGO\'s by your Donations, Thank You.'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text(
+                      'Donate',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ListTileSelectExample()));
+                    },
+                  ),
+                  const SizedBox(width: 8),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
